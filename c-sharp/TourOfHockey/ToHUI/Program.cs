211a6934e88bdf9b3ea60462c41e955e34598a71
Michael Loughman <mcloughman@gmail.com>
2021-02-19 19:32:12 -0800

    Stuffed the main method with ugly code that, while ugly...IS functional

--- c-sharp/TourOfHockey/ToHUI/Program.cs
+++ c-sharp/TourOfHockey/ToHUI/Program.cs
@@ -1,12 +1,32 @@
 ﻿using System;
+using ToHModels;
 
 namespace ToHUI
 {
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        {   
+            //Create hockeyplayer method/logic
+            HockeyPlayer newHockeyPlayer = new HockeyPlayer();
+            Console.WriteLine("Eneter a hockey player name: ");
+            newHockeyPlayer.HockeyPlayerName = Console.ReadLine();
+            Console.WriteLine("Enter Overall value: ");
+            newHockeyPlayer.SkillLevel = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Best Weapon details");
+            BestWeapon newBestWeapon = new BestWeapon();
+
+            Console.WriteLine("Enter Best Weapon name: ");
+            newBestWeapon.Name = Console.ReadLine();
+            Console.WriteLine("Enter Best Weapon description ");
+            newBestWeapon.Description = Console.ReadLine();
+            Console.WriteLine("Enter Best Weapon Speed: ");
+            newBestWeapon.Speed = int.Parse(Console.ReadLine());
+            newHockeyPlayer.BestWeapon = newBestWeapon;
+            Console.WriteLine("What country id the player from: ");
+            newHockeyPlayer.CountryName = Enum.Parse<Country>(Console.ReadLine());
+            Console.WriteLine(newHockeyPlayer.HockeyPlayerName);
+            Console.WriteLine($"Hockey Player created with details: \n\t name: {newHockeyPlayer.HockeyPlayerName} \n\t best weapon: {newHockeyPlayer.BestWeapon.Name} \n\t country: {newHockeyPlayer.CountryName}");
         }
     }
 }
